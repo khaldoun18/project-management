95cc3e611a397c2388bd22bf0e1c202ade9dbd55
khaldoun18 <84587554+khaldoun18@users.noreply.github.com>
2024-04-24 16:12:50 +0300

    added task  crud

--- resources/js/Pages/Tasks/Index.jsx
+++ resources/js/Pages/Tasks/Index.jsx
@@ -1,26 +1,38 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head, Link } from "@inertiajs/react";
 
-import { Head } from '@inertiajs/react';
+import TasksTable from "./TasksTable";
 
-
-import TasksTable from './TasksTable';
-
-export default function Dashboard({ auth, tasks, queryParams = null }) {
-
-
+export default function Index({ auth, success, tasks, queryParams = null }) {
   return (
     <AuthenticatedLayout
       user={auth.user}
-      header={<h2 className="text-xl font-semibold leading-tight text-gray-800">Tasks</h2>}
+      header={
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-semibold leading-tight text-gray-800 ">
+            Tasks
+          </h2>
+          <Link
+            href={route("task.create")}
+            className="px-3 py-1 text-white transition-all rounded shadow bg-emerald-500 hover:bg-emerald-600"
+          >
+            Add new
+          </Link>
+        </div>
+      }
     >
       <Head title="Tasks" />
 
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">
-            <TasksTable tasks={tasks} queryParams={queryParams} goTo='task.index'/>
+            <div className="p-6 text-gray-900 ">
+              <TasksTable
+                tasks={tasks}
+                queryParams={queryParams}
+                success={success}
+              />
             </div>
           </div>
         </div>
